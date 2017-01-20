767d62ef53b9373e1e53de34ee7431a269ebdca1
Norsi <natalieannorsi@gmail.com>
2017-01-20 14:28:50 -0800

    Basic canvas setup

---
+++ scripts/sketch.js
@@ -0,0 +1,12 @@
+//Created and maintained by Natalie Orsi at http://natalieorsi.net
+
+
+
+function setup() {
+  size(window.innerWidth, window.innerHeight);
+}
+
+function draw() {
+  background(255);
+}
+
